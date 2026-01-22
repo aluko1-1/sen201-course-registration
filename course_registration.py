cbccb56219a59415bab2a7e459aa2ff401477503
aluko1-1 <159243269+aluko1-1@users.noreply.github.com>
2026-01-22 01:14:04 +0100

    Add course registration system implementation

---
+++ course_registration.py
@@ -0,0 +1,10 @@
+print("STUDENT COURSE REGISTRATION SYSTEM")
+
+student_name = input("Enter student name: ")
+matric_number = input("Enter matric number: ")
+courses = input("Enter courses (separated by commas): ")
+
+print("\nREGISTRATION DETAILS")
+print("Student Name:", student_name)
+print("Matric Number:", matric_number)
+print("Courses Registered:", courses)
